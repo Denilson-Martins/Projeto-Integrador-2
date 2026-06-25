6b6b573907ae772af5eb794fd25fb2cca1e1ad55
Ryan <ryanpiresmarcondes@gmail.com.br>
2026-06-25 19:23:14 -0300

    uptd

--- Projeto integrador2/Projeto integrador2/CADASTRO.xaml.cs
+++ Projeto integrador2/Projeto integrador2/CADASTRO.xaml.cs
@@ -25,9 +25,6 @@ namespace Projeto_integrador2
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
-
-            if (txbSenha & txbUser ) ;
             NavigationService.Navigate(new CadastroDeProdutos());
         }
     }
